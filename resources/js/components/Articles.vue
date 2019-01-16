408a56209a6c50578fd57ca2fc1bdc3f4937305f
amircisija <amircisija@gmail.com>
2019-01-16 07:28:36 +0100

    Made api for articles

---
+++ resources/js/components/Articles.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>Test Module</h2>
+    <div class="row">
+      <div class="card-columns">
+        <div class="card text-left" v-for="article in articles" :key="article.id">
+          <div class="card-body">
+            <h4 class="card-title">{{ article.title }}</h4>
+            <p class="card-text">{{ article.body }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      articles: [],
+      article: {
+        id: "",
+        title: "",
+        body: ""
+      },
+      article_id: "",
+      edit: false
+    };
+  },
+  created() {
+    this.fetchArticles();
+  },
+  methods: {
+    fetchArticles() {
+      fetch("api/articles")
+        .then(res => res.json())
+        .then(res => {
+          this.articles = res.data;
+        });
+    }
+  }
+};
+</script>
